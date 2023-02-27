5daf0d19ec6fc03af916c62a1cc952071da38faf
hanankaled161 <110997076+hanankaled161@users.noreply.github.com>
2023-02-27 20:39:07 +0300

    Add files via upload

---
+++ LAB3H/src/HW1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class HW1 {
+    public static void main(String[] args) {
+        int[] A={1,2,3,4,5,6,7};
+        int[] B=A;
+        System.out.println(Arrays.toString(B));
+    }
+}
